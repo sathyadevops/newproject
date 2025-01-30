d29b5080e17708c7f01d2ae0541d4710375e0145
sathyadevops <sathyadevops9@gmail.com>
2025-01-30 04:14:49 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,9 @@
+class Demo
+{
+  public static void main(String []args)
+  {
+    Sytem.out.println(" Test code ");
+    Sytem.out.println(" Test code ");
+    Sytem.out.println(" Test code ");
+  }
+}
